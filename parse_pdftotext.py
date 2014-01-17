ee1a643ed5dc049c319308cbbd72d9dab54eaf96
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-01-17 01:02:01 -0500

    whatever

--- parse_pdftotext.py
+++ parse_pdftotext.py
@@ -7,6 +7,7 @@
 #-----------------------------------------------------------------------------
 
 # WARNING: This code SUX
+# http://astroworse.com/dumbaas/1818733571
 
 from __future__ import division, print_function
 
@@ -21,16 +22,17 @@ import re
 #pattr = re.compile(r"[0-9]{3}\.[0-9]{2,3}.*$")
 pattr = re.compile(r"([0-9]{3}\.[0-9]{2,3})(.*)")
 
+expr1 = r"([A-Za-z\.]+\s(?:(?:[A-Za-z]\.)|(?:[A-Za-z]+))\s[A-Za-z]+\s*[0-9]\,.*)"
+bad_pattr1 = re.compile(expr1)
+expr2 = r"([A-Za-z\.]+\s(?:(?:[A-Za-z]\.)|(?:[A-Za-z]+))\s[A-Za-z]+\s*[0-9].*)"
+bad_pattr2 = re.compile(expr2)
+
 with open("data/aas_abstracts.json") as f:
     data = json.loads(f.read())
 
 for ii,filename in enumerate(glob.glob("data/aas*.txt")):
     with open(filename) as f:
         text = f.read()
-
-    if data.has_key(str(ii)):
-        print("skipping {}".format(filename))
-        continue
 
     nfailures = 0
     ntotal = 0
@@ -39,9 +41,24 @@ for ii,filename in enumerate(glob.glob("data/aas*.txt")):
         ntotal += 1
         title = match.groups()[1]
         title = title[2:]
+        #m = bad_pattr.search(title)
+        #if m is not None or len(title) > 500:
+        #    if m is not None:
+        #        print(m.groups())
         if len(title) > 500:
             nfailures += 1
             continue
+
+        if ',' in title and '.' in title:
+            try:
+                groups = bad_pattr1.search(title).groups()
+            except:
+                try:
+                    groups = bad_pattr2.search(title).groups()
+                except:
+                    nfailures += 1
+                    continue
+            title = title.replace(groups[0],'')
 
         presentations[jj] = dict(title=title.decode('unicode_escape').encode('ascii','ignore'))
 
